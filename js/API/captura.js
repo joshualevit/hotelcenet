fb5b73cae4f897b848d6da15d6a7ef42ed61e7b2
Josue Aguilera <joshualevit@gmail.com>
2013-06-22 16:50:44 -0500

    22
    12

--- js/API/captura.js
+++ js/API/captura.js
@@ -2,7 +2,7 @@
 function tomarFoto(){
      navigator.device.capture.captureImage( function(c){
 		 var cant = c.length;
-		 for(i=0; i<cant;i++)
+		 for(i=0; i<cant;i++);
 				foto= c[i] .fullPath;
 				$('#regFoto').attr('rel',foto);
 				$('#mFoto').html('<img src="'+foto+'" width="100%" />');
--- js/API/eventos.js
+++ js/API/eventos.js
@@ -1,2 +1,21 @@
 //Eventos
-$(document).ready(function(e){	document.addEventListener("deviceready", function(){		if(!isLogin())			window.location.href = '#login';	   $('#regSend').tap(function(){		  if($('#regNom').val()!='' && $('#regTel').val()!='' && $('#regMail').val()!='' && $('#regFoto').attr('rel')!=undefined){			  var nom = $('#regNom').val();			  var tel = $('#regTel').val();			  var mail = $('#regMail').val();			  var foto = $('#regFoto').attr('rel');			  navigator.notification.alert(nom +'\n'+ tel +'\n'+ mail, null, "Hotel","Aceptar");		  }else{			  navigator.notification.alert('Todos los campos son requeridos', null, "Hotel","Ok");		  }	   });	   $('#regFoto').tap(function(){		   tomarFoto();	   });	}, false);});function isLogin(){	return false;}
+$(document).ready(function(e){
+	document.addEventListener("deviceready", function(){
+		if(!isLogin())
+			window.location.href = '#login';
+	   $('#regSend').tap(function(){
+		  if($('#regNom').val()!='' && $('#regTel').val()!='' && $('#regMail').val()!=''){
+			  var nom = $('#regNom').val();
+			  var tel = $('#regTel').val();
+			  var mail = $('#regMail').val();
+			  navigator.notification.alert(nom +'\n'+ tel +'\n'+ mail, null, "Hotel","Aceptar");
+		  }else{
+			  navigator.notification.alert('Todos los campos son requeridos', null, "Hotel","Ok");
+		  }
+	   });
+	}, false);
+});
+
+function isLogin(){
+	return false;
+}
