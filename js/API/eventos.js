ee22479bc56d83b9b7c728edbcdf7ea9c7027b21
Josue Aguilera <joshualevit@gmail.com>
2013-06-22 16:33:18 -0500

    3
    clase 3

---
+++ js/API/eventos.js
@@ -0,0 +1,21 @@
+//Eventos
+$(document).ready(function(e){
+	document.addEventListener("deviceready", function(){
+		if(!isLogin())
+			window.location.href = '#login';
+	   $('#regSend').tap(function(){
+		  if($('#regNom').val()!='' && $('#regTel').val()!='' && $('#regMail').val()!=''){
+			  var nom = $('#regNom').val();
+			  var tel = $('#regTel').val();
+			  var mail = $('#regMail').val();
+			  navigator.notification.alert(nom +'\n'+ tel +'\n'+ mail, null, "Hotel","Aceptar");
+		  }else{
+			  navigator.notification.alert('Todos los campos son requeridos', null, "Hotel","Ok");
+		  }
+	   });
+	}, false);
+});
+
+function isLogin(){
+	return false;
+}
