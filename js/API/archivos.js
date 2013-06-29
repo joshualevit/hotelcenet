9c574709daa691b4901224aa3859fd941bd70be0
Josue Aguilera <joshualevit@gmail.com>
2013-06-29 16:53:19 -0500

    cambio 3
    cambios para obetner infor

--- js/API/archivos.js
+++ js/API/archivos.js
@@ -2,7 +2,7 @@
 function subirFoto(foto, nom){
 	var options = new FileUploadOptions();
 	options.fileKey="archivo";
-	options.fileName="Carlos.jpg";
+	options.fileName="Josue.jpg";
 
 	var ft = new FileTransfer();
 	ft.upload(foto, "http://www.igitsoft.com/pgtest.php", function(r){
